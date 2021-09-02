0871b01d68d59d8dc27583e904c1167ae8107573
Arjo van Ramshorst <arjovanramshorst@gmail.com>
2021-09-02 17:28:56 +0200

    YOLO

--- src/main/scala/community/flock/scalademo/Routes.scala
+++ src/main/scala/community/flock/scalademo/Routes.scala
@@ -5,17 +5,35 @@ import org.http4s._
 import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
 import sttp.tapir.ztapir._
 import zio._
+import zio.clock.Clock
 import zio.interop.catz._
 
 
 object Routes {
+  var name: String = null
   // corresponds to: GET /hello?name=...
-  private val helloWorldEndpoint: ZEndpoint[String, String, String] = endpoint
-    .get.in("hello" / query[String]("name"))
-    .errorOut(stringBody).out(stringBody)
+  private val helloWorldEndpoint: ZServerEndpoint[AppEnvironment, Unit, String, String] = endpoint
+    .get
+    .in("hello")
+    .errorOut(stringBody)
+    .out(stringBody)
+    .zServerLogic(_ => name match {
+      case null => UIO(s"Please tell me your name first!")
+      case _ => UIO(s"Hello, $name")
+    })
 
-  val routes: HttpRoutes[RIO[AppEnvironment, *]] = ZHttp4sServerInterpreter
-    .from(helloWorldEndpoint) { name => UIO(s"Hello, $name") }
+  private val nameEndpoint: ZServerEndpoint[AppEnvironment, String, String, String] = endpoint
+    .post
+    .in("hello" / query[String]("name"))
+    .errorOut(stringBody)
+    .out(stringBody)
+    .zServerLogic(name => {
+      this.name = name;
+      UIO(s"Nice to meet you, $name")
+    })
+
+
+  val routes: HttpRoutes[ZIO[AppEnvironment with Has[Clock.Service], Throwable, *]] = ZHttp4sServerInterpreter
+    .from(List(nameEndpoint, helloWorldEndpoint))
     .toRoutes
-
 }
